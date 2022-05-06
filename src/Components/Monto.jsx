f7677f8c6f3e5a806246bc7b8f93fab17cdc3a4b
Fede <ferrelli.mazza@gmail.com>
2022-05-05 21:56:59 -0300

    Add big screen design

--- src/Components/Monto.jsx
+++ src/Components/Monto.jsx
@@ -12,10 +12,6 @@ function Monto() {
     const [cuota, setCuota] = useState('')
     const [periodos, setPeriodos] = useState('')
    
-
-
-
-
     const calcular = () =>{
         let i = interes/100;
         let c = cuota;
@@ -26,26 +22,14 @@ function Monto() {
         setShowResult(true)   
     }
 
-    const tempMes = ()=>{
-        calcular();
-        setInteresTemp(12);
-       
-        
-    }
-
-
-    const tempAnual = ()=>{
-
-        setInteresTemp(1);
-       
-        
-    }
 
 useEffect(() => {
     calcular()
 }, [interesTemp, interes, cuota, periodos])
     
-   console.log(interes)
+const showInfo =()=>{
+    setInfo(!info)
+}
 
 
 
@@ -79,111 +63,149 @@ useEffect(() => {
           </div>
         )}
 
-        <div className=" w-full flex flex-row justify-center items-center gap-2">
-          <h1 className=" text-primary text-main text-center font-bold text-3xl my-auto">
-            Monto a Financiar
-          </h1>
+        <div className="w-full h-screen  flex">
+          <main className="sm:flex sm:flex-col sm:w-full">
+            <div className=" w-full flex flex-row justify-center items-center gap-2">
+              <h1 className=" text-primary text-main text-center font-bold text-3xl my-auto">
+                Monto a Financiar
+              </h1>
 
-          <div
-            className="w-5 h-5 text-sm rounded-full border border-primary  text-primary text-center bg-transparent my-auto"
-            onClick={() => showInfo()}
-          >
-            ?
-          </div>
-        </div>
+              <div
+                className="w-5 h-5 text-sm rounded-full border border-primary  text-primary text-center bg-transparent my-auto sm:hidden"
+                onClick={() => showInfo()}
+              >
+                ?
+              </div>
+            </div>
 
-        <div className="flex flex-col w-full my-6">
-          <form className="flex flex-col ">
-            <div className="flex flex-row">
-              <div className="my-5 mx-3 rounded-lg relative h-12 border-2 border-primary w-1/2">
-                <label
-                  id="tasa"
-                  className="absolute -top-4 left-2 text-primary bg-slate-800 px-1"
+            <section className="sm:flex sm:flex-row sm:w-3/4 sm:mx-auto">
+
+            <div className="sm:flex sm:flex-row  sm:w-1/2">
+              <div className="w-5/6 bg-slate-700 m-auto p-4 hidden sm:flex sm:flex-col shadow-lg">
+                <p className="text-center text-white text-xl font-semibold py-2">
+                  ¿Querés saber hasta qué monto podes financiar?
+                </p>
+                <p className="text-center text-white py-2 text-lg">
+                  Para ello tenes que conocer:
+                  <li className="mt-3 my-1 italic">
+                    la tasa de interés a aplicar,
+                  </li>
+                  <li className="my-1 italic">la cantidad de cuotas y</li>
+                  <li className="my-1 italic"> el monto de cada cuota.</li>
+                </p>
+
+                <div
+                  className="m-auto w-1/2 h-12 mt-4 p-1 flex  bg-primary rounded-lg text-center text-white sm:hidden"
+                  onClick={() => showInfo()}
                 >
-                  Tasa
-                </label>
-
-                <input
-                  className="bg-slate-800 text-primary rounded-lg h-full w-full outline-none px-4 text-lg"
-                  type="number"
-                  name="tasa"
-                  onChange={(e) => setInteres(e.target.value)}
-                />
-              </div>
-
-              <div className="my-5 mx-3 rounded-lg relative h-12 border-2 border-primary w-1/2 flex flex-row justify-center items-center overflow-hidden">
-
-                <div className={interesTemp === 12 ? "w-1/2 h-full flex justify-center text-center bg-primary text-slate-800" : "w-1/2 h-full flex justify-center text-center bg-slate-800 text-primary "}  onClick={()=>tempMes()}>
-                  <span className="m-auto"> mensual</span>
-                </div>
-
-                <div className= {interesTemp === 1 ? "w-1/2 h-full flex justify-center text-center bg-primary text-slate-800 text-lg" : "w-1/2 h-full flex justify-center text-center bg-slate-800 text-primary "}   onClick={()=>tempAnual()}>
-                  <span className="m-auto"> anual</span>
+                  {" "}
+                  <div className="m-auto text-slate-800 uppercase font-bold text-lg">
+                    Ok!
+                  </div>
                 </div>
               </div>
             </div>
 
-            <div className="my-5 mx-3 rounded-lg relative h-12 border-2 border-primary">
-              <label
-                id="periodos"
-                className="absolute -top-4 left-2 text-primary bg-slate-800 px-1"
-              >
-                Periodos
-              </label>
+            <div className="flex flex-col w-full my-6 sm:w-1/2">
+              <form className="flex flex-col ">
+                <div className="flex flex-row">
+                  <div className="my-5 mx-3 rounded-lg relative h-12 border-2 border-primary w-1/2">
+                    <label
+                      id="tasa"
+                      className="absolute -top-4 left-2 text-primary bg-slate-800 px-1"
+                    >
+                      Tasa
+                    </label>
 
-              <input
-                className="bg-slate-800 text-primary rounded-lg h-full w-full outline-none px-4 text-lg"
-                type="number"
-                name="periodos"
-                onChange={(e) => setPeriodos(e.target.value)}
-              />
+                    <input
+                      className="bg-slate-800 text-primary rounded-lg h-full w-full outline-none px-4 text-lg"
+                      type="number"
+                      name="tasa"
+                      onChange={(e) => setInteres(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="my-5 mx-3 rounded-lg relative h-12 border-2 border-primary w-1/2 flex flex-row justify-center items-center overflow-hidden">
+                    <div
+                      className={
+                        interesTemp === 12
+                          ? "w-1/2 h-full flex justify-center text-center cursor-pointer bg-primary text-slate-800"
+                          : "w-1/2 h-full flex justify-center text-center cursor-pointer bg-slate-800 text-primary "
+                      }
+                      onClick={() => setInteresTemp(12)}
+                    >
+                      <span className="m-auto"> mensual</span>
+                    </div>
+
+                    <div
+                      className={
+                        interesTemp === 1
+                          ? "w-1/2 h-full flex justify-center text-center bg-primary text-slate-800 text-lg cursor-pointer"
+                          : "w-1/2 h-full flex justify-center text-center cursor-pointer bg-slate-800 text-primary "
+                      }
+                      onClick={() => setInteresTemp(1)}
+                    >
+                      <span className="m-auto"> anual</span>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="my-5 mx-3 rounded-lg relative h-12 border-2 border-primary">
+                  <label
+                    id="periodos"
+                    className="absolute -top-4 left-2 text-primary bg-slate-800 px-1"
+                  >
+                    Periodos
+                  </label>
+
+                  <input
+                    className="bg-slate-800 text-primary rounded-lg h-full w-full outline-none px-4 text-lg"
+                    type="number"
+                    name="periodos"
+                    onChange={(e) => setPeriodos(e.target.value)}
+                  />
+                </div>
+
+                <div className="my-5 mx-3 rounded-lg relative h-12 border-2 border-primary">
+                  <label
+                    id="cuota"
+                    className="absolute -top-4 left-2 text-primary bg-slate-800 px-1"
+                  >
+                    Cuota a Pagar
+                  </label>
+
+                  <input
+                    className="bg-slate-800 text-primary rounded-lg h-full w-full outline-none px-4 text-lg"
+                    type="number"
+                    name="cuota"
+                    onChange={(e) => setCuota(e.target.value)}
+                  />
+                </div>
+              </form>
             </div>
+            </section>
 
-            <div className="my-5 mx-3 rounded-lg relative h-12 border-2 border-primary">
-              <label
-                id="cuota"
-                className="absolute -top-4 left-2 text-primary bg-slate-800 px-1"
-              >
-                Cuota a Pagar
-              </label>
-
-              <input
-                className="bg-slate-800 text-primary rounded-lg h-full w-full outline-none px-4 text-lg"
-                type="number"
-                name="cuota"
-                onChange={(e) => setCuota(e.target.value)}
-              />
-            </div>
-
-            {/* <div
-              className="m-auto w-1/2 h-12 mt-4 p-1 flex  bg-primary rounded-lg text-center text-white"
-              onClick={() => calcular()}
-            >
-              {" "}
-              <div className="m-auto text-slate-800 uppercase font-bold text-lg">
-                Calcular
+            {cuota !== "" && interes !== "" && periodos !== "" && (
+              <div className="m-8 rounded-lg p-4 text-white text-lg text-center border border-primary sm:w-1/3 sm:mx-auto">
+                Pagando {periodos} cuotas de{" "}
+                {new Intl.NumberFormat("de-DE", {
+                  style: "currency",
+                  currency: "ARS",
+                }).format(cuota)}{" "}
+                al {Math.round(interes * interesTemp * 10) / 10}% anual podés
+                financiar{" "}
+                <span className="text-xl font-bold text-primary">
+                  $
+                  {new Intl.NumberFormat("de-DE", {
+                    style: "currency",
+                    currency: "ARS",
+                  }).format(va)}
+                </span>
               </div>
-            </div> */}
-          </form>
+            )}
+            
+          </main>
         </div>
-
-        {cuota !== '' && interes !== '' &&  periodos !== '' &&(
-          <div className="m-8 rounded-lg p-4 text-white text-lg text-center border border-primary">
-            Pagando {periodos} cuotas de{" "}
-            {new Intl.NumberFormat("de-DE", {
-              style: "currency",
-              currency: "ARS",
-            }).format(cuota)}{" "}
-            al {Math.round(interes*interesTemp*10)/10}% anual podés financiar{" "}
-            <span className="text-xl font-bold text-primary">
-              $
-              {new Intl.NumberFormat("de-DE", {
-                style: "currency",
-                currency: "ARS",
-              }).format(va)}
-            </span>
-          </div>
-        )}
       </div>
     );
 }
