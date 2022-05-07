17afdae2bc4e1e0ad090a45fd15d2af6a5188fc0
Fede <ferrelli.mazza@gmail.com>
2022-05-06 21:32:23 -0300

    Add comments

--- src/Components/Monto.jsx
+++ src/Components/Monto.jsx
@@ -1,86 +1,91 @@
-import React, {useState, useEffect} from 'react';
-import Resumen from './Resumen';
-
+import React, { useState, useEffect } from "react";
+import Resumen from "./Resumen";
 
 function Monto() {
+  const [info, setInfo] = useState(false);
+  const [showResult, setShowResult] = useState(false);
 
-    const [info, setInfo] = useState(false)
-    const [showResult, setShowResult] = useState(false)
+  const [interes, setInteres] = useState("");
+  const [interesTemp, setInteresTemp] = useState(12);
+  const [va, setVA] = useState();
+  const [cuota, setCuota] = useState("");
+  const [periodos, setPeriodos] = useState("");
 
-    const [interes, setInteres] = useState('')
-    const [interesTemp, setInteresTemp] = useState(12)
-    const [va, setVA] = useState()
-    const [cuota, setCuota] = useState('')
-    const [periodos, setPeriodos] = useState('')
-   
-    const calcular = () =>{
-        let i = interes/100;
-        let c = cuota;
-        let n = periodos;
-        let m = interesTemp;
-        let valorActual = (c/(i/(12/m)))*(1-(1/((1+i/(12/m))**n)));
-        setVA(valorActual)
-        setShowResult(true)   
-    }
+/* Para mobile: toggle para mostrar explicacion */
+  const showInfo = () => {
+    setInfo(!info);
+  };
 
+  
+ /* Calcula el resultado de la pantalla */ 
+  const calcular = () => {
+    let i = interes / 100;
+    let c = cuota;
+    let n = periodos;
+    let m = interesTemp;
+    let valorActual = (c / (i / (12 / m))) * (1 - 1 / (1 + i / (12 / m)) ** n);
+    setVA(valorActual);
+    setShowResult(true);
+  };
 
-useEffect(() => {
-    calcular()
-}, [interesTemp, interes, cuota, periodos])
-    
-const showInfo =()=>{
-    setInfo(!info)
-}
-
+  useEffect(() => {
+    calcular();
+  }, [interesTemp, interes, cuota, periodos]);
 
 
-    return (
-      <div className=" bg-slate-800 w-full">
-        {info && (
-          <div className="w-full h-screen fixed z-10 bg-slate-700/70 flex">
-            <div className="w-11/12 bg-slate-800 m-auto p-4 shadow-lg">
-              <p className="text-center text-white text-xl font-semibold py-2">
-                ¿Querés saber hasta qué monto podes financiar?
-              </p>
-              <p className="text-center text-white py-2 text-lg">
-                Para ello tenes que conocer:
-                <li className="mt-3 my-1 italic">
-                  la tasa de interés a aplicar,
-                </li>
-                <li className="my-1 italic">la cantidad de cuotas y</li>
-                <li className="my-1 italic"> el monto de cada cuota.</li>
-              </p>
 
-              <div
-                className="m-auto w-1/2 h-12 mt-4 p-1 flex  bg-primary rounded-lg text-center text-white"
-                onClick={() => showInfo()}
-              >
-                {" "}
-                <div className="m-auto text-slate-800 uppercase font-bold text-lg">
-                  Ok!
-                </div>
+  return (
+    <div className=" bg-slate-800 w-full">
+      {/* Para mobile: explicación de los requisitos para utilizar esta pantalla */
+        info && (
+        <div className="w-full h-screen fixed z-10 bg-slate-700/70 flex">
+          <div className="w-11/12 bg-slate-800 m-auto p-4 shadow-lg">
+            <p className="text-center text-white text-xl font-semibold py-2">
+              ¿Querés saber hasta qué monto podes financiar?
+            </p>
+            <p className="text-center text-white py-2 text-lg">
+              Para ello tenes que conocer:
+              <li className="mt-3 my-1 italic">
+                la tasa de interés a aplicar,
+              </li>
+              <li className="my-1 italic">la cantidad de cuotas y</li>
+              <li className="my-1 italic"> el monto de cada cuota.</li>
+            </p>
+
+            <div
+              className="m-auto w-1/2 h-12 mt-4 p-1 flex  bg-primary rounded-lg text-center text-white"
+              onClick={() => showInfo()}
+            >
+              {" "}
+              <div className="m-auto text-slate-800 uppercase font-bold text-lg">
+                Ok!
               </div>
             </div>
           </div>
-        )}
+        </div>
+      )}
 
-        <div className="h-screen w-full mx-auto flex">
-          <main className="sm:flex sm:flex-col sm:w-full">
-            <div className=" w-full flex flex-row justify-center items-center gap-2">
-              <h1 className=" text-primary text-main text-center font-bold text-3xl my-auto">
-                Monto a Financiar
-              </h1>
+      <div className="h-screen w-full mx-auto flex">
 
-              <div
-                className="w-5 h-5 text-sm rounded-full border border-primary  text-primary text-center bg-transparent my-auto sm:hidden"
-                onClick={() => showInfo()}
-              >
-                ?
-              </div>
+        <main className="sm:flex sm:flex-col sm:w-full">
+
+{/* Titulo */}
+          <div className=" w-full flex flex-row justify-center items-center gap-2">
+            <h1 className=" text-primary text-main text-center font-bold text-3xl my-auto">
+              Monto a Financiar
+            </h1>
+
+            <div
+              className="w-5 h-5 text-sm rounded-full border border-primary  text-primary text-center bg-transparent my-auto sm:hidden"
+              onClick={() => showInfo()}
+            >
+              ?
             </div>
+          </div>
 
-            <section className="sm:flex sm:flex-row sm:w-3/4 sm:mx-auto">
+          <section className="sm:flex sm:flex-row sm:w-3/4 sm:mx-auto">
 
+{/* Para laptop: explicación de los requisitos para utilizar esta pantalla */}
             <div className="sm:flex sm:flex-row  sm:w-1/2">
               <div className="w-/6 bg-slate-700 m-auto p-4 hidden sm:flex sm:flex-col shadow-lg">
                 <p className="text-center text-white text-xl font-semibold py-2">
@@ -107,6 +112,7 @@ const showInfo =()=>{
               </div>
             </div>
 
+{/* Formulario */}
             <div className="flex flex-col mx-auto w-11/12 my-6 sm:w-1/2">
               <form className="flex flex-col w-full m-auto">
                 <div className="flex flex-row gap-5">
@@ -141,7 +147,7 @@ const showInfo =()=>{
                     <div
                       className={
                         interesTemp === 1
-                          ? "w-1/2 h-full flex justify-center text-center bg-primary text-slate-800 text-lg cursor-pointer"
+                          ? "w-1/2 h-full flex justify-center text-center bg-primary text-slate-800 cursor-pointer"
                           : "w-1/2 h-full flex justify-center text-center cursor-pointer bg-slate-800 text-primary "
                       }
                       onClick={() => setInteresTemp(1)}
@@ -184,10 +190,11 @@ const showInfo =()=>{
                 </div>
               </form>
             </div>
-            </section>
+          </section>
 
-            {cuota !== "" && interes !== "" && periodos !== "" && (
-                <>
+{/* Muestra el resultado de la pantalla y el resumen */}
+          {cuota !== "" && interes !== "" && periodos !== "" && (
+            <>
               <div className="w-11/12 mx-auto my-2 rounded-lg p-4 text-white text-lg text-center border border-primary sm:w-1/3 sm:mx-auto">
                 Pagando {periodos} cuotas de{" "}
                 {new Intl.NumberFormat("de-DE", {
@@ -205,23 +212,19 @@ const showInfo =()=>{
                 </span>
               </div>
 
-<Resumen 
-va={va}
-cuota={cuota}
-periodos={periodos}
-interes={interes}
-interesTemp={interesTemp}
-/>
-</>
-            )}
- 
-            
-          </main>
-          
-        </div>
- 
+              <Resumen
+                va={va}
+                cuota={cuota}
+                periodos={periodos}
+                interes={interes}
+                interesTemp={interesTemp}
+              />
+            </>
+          )}
+        </main>
       </div>
-    );
+    </div>
+  );
 }
 
-export default Monto
+export default Monto;
