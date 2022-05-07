37e0aca87b5e3e4b8c79fca95338db71c726c17e
Fede <ferrelli.mazza@gmail.com>
2022-05-07 11:05:29 -0300

    Restyle  NavBar (mobile)

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -33,8 +33,8 @@ const showNavBar = () =>{
                 <button className="bg-primary text-slate-800 p-2 rounded-lg" onClick={e=>showNavBar()}>¿Qué querés calcular?</button>
 
           { showNav &&
-          <div className="fixed z-10 top-0 left-0 right-0 w-full h-screen bg-slate-800/80" onClick={()=>setShowNav(false)}>
-          <nav className=" text-primary flex flex-col text-xl text-center bg-slate-800  py-8 shadow-xl drop-shadow-lg rounded-b-lg">
+          <div className="fixed z-10 top-0 left-0 right-0 w-full h-screen bg-white/20" onClick={()=>setShowNav(false)}>
+          <nav className=" text-primary flex flex-col text-xl text-center bg-slate-800  py-8 pt-16 shadow-xl drop-shadow-lg rounded-b-lg">
             {links.map((i) => (
               <h3 key={i.key} className="py-1" >
                 <Link to={i.link}>{i.text}</Link>
