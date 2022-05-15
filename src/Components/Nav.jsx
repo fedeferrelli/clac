62189bfaf5764d03b7692d883b47cdd453922e2f
Fede <ferrelli.mazza@gmail.com>
2022-05-15 17:36:56 -0300

    Fix Footer at Cuota a Pagar screen

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -32,7 +32,7 @@ const showNavBar = () =>{
                 <button className="bg-primary text-slate-800 p-2 rounded-lg" onClick={e=>showNavBar()}>¿Qué querés calcular?</button>
 
           { showNav &&
-          <div className="fixed z-20 top-0 left-0 right-0 w-full h-screen bg-slate-500/40" onClick={()=>setShowNav(false)}>
+          <div className="fixed z-20 top-0 bottom-0 left-0 right-0 w-full h-screen bg-slate-500/40" onClick={()=>setShowNav(false)}>
           <nav className=" text-primary flex flex-col text-xl font-bold text-center bg-slate-800  py-8 pt-16 shadow-xl drop-shadow-lg">
             {elements.map((i) => (
               <h3 key={i.key} className="py-2" >
